e8c473c2790b573ed7735d049396c368982b91b2
dawooddilawar <d.dilawar94@gmail.com>
2023-11-12 03:17:22 +0100

    - add page for balls - new design-test for jersey - add vite - extract navbar

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
-const navToggle = document.querySelector('.nav--toggle');
+function initializeNavbar() {
+    const navToggle = document.querySelector('.nav--toggle');
 
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
-})
+    if (navToggle) {
+        navToggle.addEventListener('click', () => {
+            document.body.classList.toggle('nav-open');
+        });
+    }
+}
+
+fetch('navbar.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('navbar-placeholder').innerHTML = data;
+        initializeNavbar(); // Initialize the event listener after the navbar is loaded
+    });
